624579be0817b0b493ffb022e62dd672c4d42e49
FIllipe Romero <fillipe.romero@gmail.com>
2023-07-06 16:14:08 -0600

    Add global hook to get current lesson

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,18 +1,11 @@
 import ReactPlayer from 'react-player'
 import { useDispatch } from 'react-redux'
 
-import { useAppSelector } from '../store'
-import { next } from '../store/slices/player'
+import { next, useCurrentLesson } from '../store/slices/player'
 
 export function Video() {
   const dispatch = useDispatch()
-  const lesson = useAppSelector(state => {
-    const { currentLessonIndex, currentModuleIndex } = state.player
-
-    const currentLesson = state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
-
-    return currentLesson
-  })
+  const { currentLesson } = useCurrentLesson()
 
   function handlePlayNext() {
     dispatch(next())
@@ -26,7 +19,7 @@ export function Video() {
         controls
         onEnded={handlePlayNext}
         playing
-        url={`https://www.youtube.com/watch?v=${lesson.id}`}
+        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
       />
     </div>
   )
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -4,9 +4,16 @@ import { Video } from '../components/Video'
 import { Header } from '../components/Header'
 import { Module } from '../components/Module'
 import { useAppSelector } from '../store'
+import { useCurrentLesson } from '../store/slices/player'
+import { useEffect } from 'react'
 
 export function Player() {
   const modules = useAppSelector(state => state.player.course.modules)
+  const { currentLesson } = useCurrentLesson()
+
+  useEffect(() => {
+    document.title = `Watching: ${currentLesson.title}`
+  }, [currentLesson.title])
 
   return (
     <div className='h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center'>
